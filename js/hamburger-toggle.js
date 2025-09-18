064102b7dcd0c067f7e649274a1bee46180e3b7b
Rijnder Wever <rien334@gmail.com>
2025-09-18 11:17:16 +0200

    js: remove unneeded event listener
    if you `defer` there is no need to add a listener for the DOM being finished

--- js/hamburger-toggle.js
+++ js/hamburger-toggle.js
@@ -1,30 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menu = document.getElementById("mobile-menu");
-    const hamburger = document.getElementById("hamburger-menu");
-    const closeBtn = document.getElementById("close-btn");
-    const anchorLinks = menu.querySelectorAll("a[href*='#']");
+const menu = document.getElementById("mobile-menu");
+const hamburger = document.getElementById("hamburger-menu");
+const closeBtn = document.getElementById("close-btn");
+const anchorLinks = menu.querySelectorAll("a[href*='#']");
 
-    function toggleMenu() {
-        menu.setAttribute('aria-hidden',
-                          menu.getAttribute('aria-hidden') === 'true' ? 'false' : 'true');
-        menu.classList.toggle("open");
-        document.body.classList.toggle("no-scroll");
-    }
+function toggleMenu() {
+    menu.setAttribute('aria-hidden',
+                      menu.getAttribute('aria-hidden') === 'true' ? 'false' : 'true');
+    menu.classList.toggle("open");
+    document.body.classList.toggle("no-scroll");
+}
 
-    hamburger.addEventListener("click", (e) => {
-        e.preventDefault();
+hamburger.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleMenu();
+
+});
+
+closeBtn.addEventListener("click", () => {
+    toggleMenu();
+});
+
+// Close menu when clicking on a same-page link within the menu
+anchorLinks.forEach(link => {
+    link.addEventListener("click", () => {
         toggleMenu();
-
-    });
-
-    closeBtn.addEventListener("click", () => {
-                toggleMenu();
-    });
-
-    // Close menu when clicking on a same-page link within the menu
-    anchorLinks.forEach(link => {
-        link.addEventListener("click", () => {
-            toggleMenu();
-        });
     });
 });
--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,17 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const html = document.documentElement;
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-        html.setAttribute("data-theme", savedTheme);
-    }
-    const themeToggle = document.getElementById("theme-toggle");
+const html = document.documentElement;
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+    html.setAttribute("data-theme", savedTheme);
+}
+const themeToggle = document.getElementById("theme-toggle");
 
-    themeToggle.addEventListener("click", (event) => {
-        event.preventDefault(); // Prevents page reload if it's a link
-        const currentTheme = html.getAttribute("data-theme");
-        const newTheme = currentTheme === "dark" ? "light" : "dark";
+themeToggle.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevents page reload if it's a link
+    const currentTheme = html.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
 
-        html.setAttribute("data-theme", newTheme);
-        localStorage.setItem("theme", newTheme);
-    });
+    html.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
 });
